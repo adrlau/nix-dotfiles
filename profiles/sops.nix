910cd3316bfaf10f720c16264df366cb554a77f9
Adrian G L <adrian@lauterer.it>
2024-11-16 15:06:45 +0100

    fix: sops global to relative path

--- profiles/sops.nix
+++ profiles/sops.nix
@@ -10,21 +10,22 @@
   # This will add secrets.yml to the nix store
   # You can avoid this by adding a string to the full path instead, i.e.
   # sops.defaultSopsFile = "/root/.sops/secrets/example.yaml";
-  sops.defaultSopsFile = "/etc/nixos/nix-dotfiles/secrets/secrets.yaml";
-  sops.validateSopsFiles = false;
-  # This will automaticx-sopsally import SSH keys as age keys
-  sops.age.sshKeyPaths = [
+  # sops.defaultSopsFile = "/etc/nixos/nix-dotfiles/secrets/secrets.yaml";
+  sops = {
+    defaultSopsFile = ../secrets/secrets.yaml;
+    validateSopsFiles = false;
+
+    # This will automaticx-sopsally import SSH keys as age keys
+    age.sshKeyPaths = [
 			  "/etc/ssh/nixos"
 			  "/root/.ssh/nixos"
-			  #"/$HOME/.ssh/nixos"
-        #"/home/gunalx/.ssh/nixos"
 			];
-  # This is using an age key that is expected to already be in the filesystem
-  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
-  # This will generate a new key if the key specified above does not exist
-  sops.age.generateKey = true;
-
-  # This is the actual specification of the secrets.
-  #sops.secrets."myservice/my_subdir/my_secret" = {};
-
+    # This is using an age key that is expected to already be in the filesystem
+    #age.keyFile = "/var/lib/sops-nix/key.txt";
+    age.keyFile = "/root/.config/sops/age/key.txt";
+    # This will generate a new key if the key specified above does not exist
+    age.generateKey = true;
+    # This is the actual specification of the secrets.
+    #secrets."myservice/my_subdir/my_secret" = {};
+  };
 }
