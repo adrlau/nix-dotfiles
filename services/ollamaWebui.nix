8ca7bb1890597cd5ee339cc2f8170a4bd00042fa
Adrian Gunnar Lauterer <adrian@lauterer.it>
2025-01-22 12:25:26 +0100

    update ollama webui

--- services/ollamaWebui.nix
+++ services/ollamaWebui.nix
@@ -6,7 +6,11 @@
 
   ];
 
+  services.tika.enable=true;
+  services.tika.openFirewall=true;
+  services.tika.listenAddress = "localhost";
 
+  
   services.open-webui = {
     enable = true;
 
@@ -14,6 +18,13 @@
     port = 11111;
     host = "0.0.0.0";
     openFirewall = true;
+
+    enviroment = {
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+      PDF_EXTRACT_IMAGES = "False";
+    };
 
   };
 }
