0a63318ddcfc61038c3a9aba535bc93441c10129
Adrian Gunnar Lauterer <adrian@lauterer.it>
2025-05-24 23:57:00 +0200

    feat: add common font configuration and packages for home environment

---
+++ home/common/fonts.nix
@@ -0,0 +1,31 @@
+{ pkgs, lib, ... }:
+{
+  
+  home.packages = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-emoji
+        liberation_ttf
+        fira-code
+        fira-code-symbols
+        ubuntu_font_family
+        zpix-pixel-font
+        font-awesome
+        font-awesome_5
+        font-awesome_4
+
+    ]++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues nerd-fonts);
+
+
+  fonts.fontconfig.enable = true;
+  fonts.fontconfig.defaultFonts = {
+      emoji = ["noto-fonts-emoji font-awesome"];
+      monospace = ["0xproto" "zpix" "fira-code"];
+      serif = [ "Noto Serif" "Source Han Serif" ];
+      sansSerif = [ "Noto Sans" "Source Han Sans" ];
+  };
+
+ 
+
+}
+
